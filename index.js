60d9ec995d10c34f8c0c3f67feb51e39066c794b
mxvilla <maximiliano.villa@gmail.com>
2023-04-08 02:17:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ app.get('/gpt/:text', async (req, res) => {
       //CHAT MODE EXECUTION
 
       Add user message to  messages
-      //messages.push({role: "user", content: text})
+      messages.push({role: "user", content: text})
       //Check if message history is exceeded
       console.log("Conversations in History: " + ((messages.length / 2) -1) + "/" + process.env.HISTORY_LENGTH)
       if(messages.length > ((process.env.HISTORY_LENGTH * 2) + 1)) {
